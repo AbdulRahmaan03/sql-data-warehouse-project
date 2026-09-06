9ee90a25e70e2e33fbb3228052d7d9a4a5daa70a
Abdul Rahmaan Ansari <aabdulrahmaan15@gmail.com>
2026-09-06 23:14:59 +0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+
+Create Database and Schemas
+
+*/
+
+use master;
+
+create database DataWarehouse;
+
+use DataWarehouse;
+
+create schema bronze;
+Go
+create schema silver;
+Go
+create schema gold;
+Go
