8e2fd8929c18451fb46bc1b1f87934591176a8b1
Abdul Rahmaan Ansari <aabdulrahmaan15@gmail.com>
2026-09-07 10:55:10 +0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,15 +1,28 @@
 /*
 
 Create Database and Schemas
+If the database exists, it is dropped and recreated.
 
 */
 
 use master;
+Go
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
 create database DataWarehouse;
-
+Go
+  
 use DataWarehouse;
+Go
 
+-- Create Schemas
 create schema bronze;
 Go
 create schema silver;
